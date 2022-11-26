2b75b7d5d4b9769d6d71585f0a238c536bda4c99
manojpi <111719118+manojpi@users.noreply.github.com>
2022-11-26 14:23:35 -0600

    Add files via upload
    Added the Document schema for users

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// importing mongoose
 const mongoose = require("mongoose");
 
 const options = {
@@ -19,14 +20,73 @@ const options = {
     useUnifiedTopology: true,
 };
 
-const dbUrl = "mongodb+srv://project:project@cluster0.qatbpfc.mongodb.net/?retryWrites=true&w=majority"
+// Database username and password for database access in our mongoose
+const dbUname = "project"
+const dbPwd = "project"
+const dbUrl = `mongodb+srv://${dbUname}:${dbPwd}@cluster0.qatbpfc.mongodb.net/?retryWrites=true&w=majority`
 
+// connecting to our database
 mongoose.connect(dbUrl, options, (err) => {
     if (err) console.log(err);
 })
 
+// validating our mongoose connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Mongo DB has been connected succssfully");
 })
+
+// creating a schema for for database
+let Schema = mongoose.Schema;
+let userSchema = new Schema(
+    {
+        name : {
+            type: String
+        },
+        email : {
+            type: String
+        },
+        phoneNumber : {
+            type: Number
+        },
+        password : {
+            type: String
+        }
+    }
+);
+
+let UserModel = mongoose.model("user", userSchema);
+
+app.post('/register', async (req, res) => {
+    const inputUserInfo = req.body;
+    console.log(inputUserInfo);
+    console.log("Yes")
+    
+    // let users = await UserModel.find();
+    // const userWithSameEmail = users.filter( user => user.email === inputUserInfo.email).length;
+    // if (userWithSameEmail) {
+    //     res.status(500).send(`User with email: ${inputUserInfo.email} already exits.`);
+    // }
+    // else {
+    //     try {
+    //         console.log('User Info:', inputUserInfo);
+    //         let user = new UserModel(inputUserInfo);
+    //         user = user.save();
+    //         res.status(200).json({
+    //             status:400,
+    //             data:user,
+    //         });
+
+    //     }
+    //     catch {
+    //         res.status(400).json({
+    //             status:400,
+    //             data:user,
+    //         });
+    //     }
+    // }
+         
+});
+
+app.listen(PORT)
